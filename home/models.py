4d20b07968526f328b65a7b872e810164c668e57
Crowdbotics <team@crowdbotics.com>
2020-03-20 10:14:19 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
@@ -6,18 +7,19 @@ from django.db import models
 
 
 class CustomText(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(max_length=150,)
+    name = models.BinaryField(null=True, blank=True,)
 
     def __str__(self):
         return self.title
 
     @property
     def api(self):
-        return f'/api/v1/customtext/{self.id}/'
+        return f"/api/v1/customtext/{self.id}/"
 
     @property
     def field(self):
-        return 'title'
+        return "title"
 
 
 class HomePage(models.Model):
@@ -25,8 +27,23 @@ class HomePage(models.Model):
 
     @property
     def api(self):
-        return f'/api/v1/homepage/{self.id}/'
+        return f"/api/v1/homepage/{self.id}/"
 
     @property
     def field(self):
-        return 'body'
+        return "body"
+
+
+class Testing(models.Model):
+    "Generated Model"
+    test = models.BigIntegerField()
+
+
+class Test(models.Model):
+    "Generated Model"
+    test = models.BigIntegerField()
+
+
+class Testtt(models.Model):
+    "Generated Model"
+    testt = models.BinaryField()
